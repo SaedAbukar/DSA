d67f869273ec529f574a703abc458e6f10e4aba8
Saed Abukar <saed.a.abukar@gmail.com>
2025-04-09 12:53:50 +0300

    done

---
+++ StackAndQueues/exec5.py
@@ -0,0 +1,19 @@
+def get_pairs(list):
+    even = Queue()
+    odd = Queue()
+    pairs = []
+    for i in list:
+        if i % 2 == 0:
+            if odd._size > 0:
+                n = odd.dequeue()
+                pairs.append((n, i))
+            else:
+                even.enqueue(i)
+        else:
+            if even._size > 0:
+                n = even.dequeue()
+                pairs.append((n, i))
+            else:
+                odd.enqueue(i)
+                
+    return pairs
